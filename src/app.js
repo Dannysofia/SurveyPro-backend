510dc132e5f082a5b500987ba496a24fc46047b2
Mario Diaz <mariomd2807@gmail.com>
2025-10-14 22:12:06 -0500

    Conección de rutas para datos de encuestas

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 // Importar rutas
 const userRoutes = require('./routes/userRoutes');
 const authRoutes = require('./routes/authRoutes');
+const getSurveysRoutes = require('./routes/getSurveysRoutes');
+const profileRoutes = require('./routes/profileRoutes');
 const surveyRoutes = require('./routes/surveyRoutes');
 const questionTypeRoutes = require('./routes/questionTypeRoutes');
 
@@ -18,6 +20,8 @@ app.use(express.json());
 // Usar las rutas de usuarios
 app.use('/usuarios', userRoutes);
 app.use('/auth', authRoutes);
+app.use('/surveys', getSurveysRoutes);
+app.use('/profile', profileRoutes);
 app.use('/encuestas', surveyRoutes);
 app.use('/tipos-pregunta', questionTypeRoutes);
 
